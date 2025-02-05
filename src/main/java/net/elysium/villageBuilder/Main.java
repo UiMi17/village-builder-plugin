0f863e65cc26fdcaff89e45aec494d6afdf736f5
Dan Dudaiev <roffrooff@gmail.com>
2025-02-05 14:30:54 +0200

    Project INIT;

---
+++ src/main/java/net/elysium/villageBuilder/Main.java
@@ -0,0 +1,16 @@
+package net.elysium.villageBuilder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
